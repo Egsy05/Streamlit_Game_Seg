9bd0e0e9ea9e83a7f5178456deec31dc5ea329af
Egsy05 <162001169+Egsy05@users.noreply.github.com>
2024-06-05 02:54:21 +0700

    Update 3_Steam Games Clusters.py

--- pages/3_Steam Games Clusters.py
+++ pages/3_Steam Games Clusters.py
@@ -4,10 +4,10 @@ import pandas as pd
 data = pd.read_csv("games_labels.csv")
 
 st.header("This is the clusters for All games in Steam from 2008 to 2023", divider='blue')
-cluster_value = data['clusters'].unique()
+cluster_value = data['Cluster'].unique()
 select_clusters = st.multiselect("Select Cluster",cluster_value)
 if select_clusters:
-    filter_table = data[data['clusters'].isin(select_clusters)]
+    filter_table = data[data['Cluster'].isin(select_clusters)]
     st.write(filter_table)
 else:
     filter_table = data
