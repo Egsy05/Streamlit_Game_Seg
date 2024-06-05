5adef188fa54e631c4d48d8828dac075665b442c
Egsy05 <162001169+Egsy05@users.noreply.github.com>
2024-06-05 14:14:18 +0700

    delete data since release change scatter x=profit

--- pages/3_Steam Games Clusters.py
+++ pages/3_Steam Games Clusters.py
@@ -4,7 +4,6 @@ import pandas as pd
 data = pd.read_csv("games_labels.csv")
 data['Review'] = data['Positive']-data['Negative']
 data['Profit'] = data['Peak CCU']*data['Price']
-data['Since_Release'] = max(data['Release_date'])-data['Release_date']
 st.header("This is the clusters for All games in Steam from 2008 to 2023", divider='blue')
 cluster_value = data['Labels'].sort_values().unique()
 select_clusters = st.multiselect("Select Cluster",cluster_value)
@@ -16,4 +15,4 @@ else:
     st.write(filter_table)
 
 
-st.scatter_chart(filter_table, x='Price', y='Peak CCU', color='Labels',size='Review')
+st.scatter_chart(filter_table, x='Profit', y='Peak CCU', color='Labels',size='Review')
